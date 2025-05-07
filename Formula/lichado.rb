e7b97f32fbded8cc3ce39fb08969a7ff3c0f3093
GitHub Action <action@users.noreply.github.com>
2025-05-07 14:16:46 +0000

    Add/update Homebrew formula for v1.0.0

--- Formula/lichado.rb
+++ Formula/lichado.rb
@@ -7,20 +7,20 @@ class Lichado < Formula
   # Define all platform-specific URLs and SHA256 checksums
   if Hardware::CPU.intel?
     url "https://github.com/whiteducksoftware/azure-devops-license-checker/releases/download/v1.0.0/lichado-osx-x64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "373827a93e767b0eccecaa3f5087164d9bbd6d00dc258b6c268c4424e4fbdab4"
   elsif Hardware::CPU.arm?
     url "https://github.com/whiteducksoftware/azure-devops-license-checker/releases/download/v1.0.0/lichado-osx-arm64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "4360bad1fdc05cf482be2c6e9fcbde1c1357798540d4dd4bdf92c1b6cc2c3162"
   end
 
   # Linux (assuming x64)
   if OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/whiteducksoftware/azure-devops-license-checker/releases/download/v1.0.0/lichado-linux-x64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "b917ae85469fd133435ed3950c697b1c043a767902bb33a0c5b4ee72ab07a991"
     elsif Hardware::CPU.arm?
       url "https://github.com/whiteducksoftware/azure-devops-license-checker/releases/download/v1.0.0/lichado-linux-arm64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "e80d533d41bb3a8b00379050a072b7c96f2fb4c6cb0fd394e075044885723ef6"
     end
   end
 
